fbdef6aa19da7a3b3e7d47e20e364684ed396674
maalcantara <marigigialcantara40@gmail.com>
2023-10-06 17:01:28 -0300

    feat: create a global loading component

---
+++ src/components/UI/Loading/index.tsx
@@ -0,0 +1,9 @@
+export const Loading = () => {
+  return (
+    <div className="flex h-screen justify-center items-center">
+      <div className="p-4 text-orange-500 text-base">
+        <h1>Loading...</h1>
+      </div>
+    </div>
+  );
+};
